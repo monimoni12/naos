3b1cf55c29ec1990392caaf9a1eaa765496ea9f7
Eunseo (Monica) Lee <monimoni@ewhain.net>
2025-09-17 15:56:54 +0900

    real init

---
+++ be/src/main/java/com/moni/naos/NaosApplication.java
@@ -0,0 +1,13 @@
+package com.moni.naos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaosApplication.class, args);
+	}
+
+}
